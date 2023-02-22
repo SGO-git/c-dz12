f8f9d5c7a3449f9502f4c284114c20c657512d95
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-02-22 20:28:34 +0500

    add dz12

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Math.Abs(Convert.ToInt32(System.Console.ReadLine()));
+}
+
+int Sum(int number)
+{
+    int result = 0;
+    while (number >= 1)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+int num = ReadInt("Imput number ");
+System.Console.WriteLine(Sum(num));
